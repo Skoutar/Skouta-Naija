cc034fad2224ee9ae4c34cb91e3dcc7133b08838
timmy <akandeolawole@gmail.com>
2024-06-14 16:11:36 +0100

    how skoutar works done

--- src/Components/Landingpage/Landpage.jsx
+++ src/Components/Landingpage/Landpage.jsx
@@ -26,7 +26,7 @@ const Landpage = () => {
         </div>
       </div>
       {/* Discover section done */}
-      
+
       {/* Marquee section start */}
       <div className='marque-section'>
         <h3 className='margue-shot'>Recently Shot With Us</h3>
@@ -55,12 +55,69 @@ const Landpage = () => {
         </Marquee>
       </div>
       {/* Marquee section ends */}
-      
+
       {/* Find space starts */}
       <div className='find-space'>
-        <CarouselComponent/>
+        <CarouselComponent />
       </div>
       {/* Find space ends */}
+      {/* Created by skoukar */}
+      <div className='created'>
+        <div className='created-text'>
+          <h1 className='created-header'>Created With Skoutar</h1>
+          <p className='created-p'>Better Locations, Endless Possibilities</p>
+        </div>
+        <div className='Created-b'>
+          <div>
+            <div className='Created-offer'>
+              <h4 className='offer-h'>Discover Amazing Film Locations</h4>
+              <p className='offer-p'>Our advanced search filters allow you to find the perfect film location for your next project. Explore verified locations with ease and read user reviews to make informed decisions.</p>
+            </div>
+            <div className='Created-offer'>
+              <h4 className='offer-h'>Verified Locations</h4>
+              <p className='offer-p'>We ensure that all our film locations are verified to meet the highest standards. You can trust that the locations listed on our platform are reliable and suitable for your needs.</p>
+            </div>
+            <div className='Created-offer'>
+              <h4 className='offer-h'>User Reviews</h4>
+              <p className='offer-p'>Read honest and unbiased reviews from other filmmakers who have used our platform. Their experiences will help you make the right choice for your film production.</p>
+            </div>
+          </div>
+          <img src="images/created.png" alt="" className='created-img' />
+        </div>
+
+      </div>
+      {/* How skoutar works */}
+      <div className='how-section'>
+        <h1 className='how-section1-h'>How Skouta Works</h1>
+        <p className='how-section1-p'>We love production and are committed to simplifying it, starting with film locations. Skouta’s marketplace connects thousands of producers and agencies with spaces to create commercials, films, and branded content throughout Nigeria.</p>
+        <div className='how-section2'>
+          <div className='section2-text'>
+            <div className='how-text'>
+              <img src="images/howicon1.png" alt="" className='section2-icon' />
+              <div  className='how-text2'>
+                <h4 className='section2-h'>High Quality Listing</h4>
+                <p className='section2-p'>Detailed descriptions, professional photos and virtual tours. From a fully-equipped studio to a seaside mansion or abandoned warehouse, you can find your perfect fit.</p>
+              </div>
+            </div>
+            <hr className='how-hr'/>
+            <div className='how-text'>
+              <img src="images/howicon2.png" alt="" className='section2-icon' />
+              <div className='how-text2'> 
+                <h4 className='section2-h'>Secure Payment System</h4>
+                <p className='section2-p'>With our clear rates and no hidden charges, you will know what you are paying for, right from booking, streamlined online transactions for location fees and permits.</p>
+              </div>
+            </div> 
+            <hr className='how-hr'/>
+            <div className='how-text'>
+              <img src="images/howicon3.png" alt="" className='section2-icon' />
+              <div  className='how-text2'>
+                <h4 className='section2-h'>Availability Calendar</h4>
+                <p className='section2-p'>Real-time availability for booking. Emergencies happen. Book locations and anything you need for any event or production with ease.</p>
+              </div></div>
+          </div>
+          <img src="images/how (2).png" alt="" className='section2-img' />
+        </div>
+      </div>
     </div>
   )
 }
