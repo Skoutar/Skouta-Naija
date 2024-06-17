aa93e5874a6b5fef8dfe13f57a61cd31165ddfd1
timmy <akandeolawole@gmail.com>
2024-06-17 12:27:28 +0100

    question-part-done

--- src/Components/Landingpage/Landpage.jsx
+++ src/Components/Landingpage/Landpage.jsx
@@ -1,4 +1,5 @@
 import CarouselComponent from '../Carousel/CarouselComponent'
+import Footer from '../Footer/Footer'
 import './Landpage.css'
 import Marquee from 'react-fast-marquee'
 
@@ -94,23 +95,23 @@ const Landpage = () => {
           <div className='section2-text'>
             <div className='how-text'>
               <img src="images/howicon1.png" alt="" className='section2-icon' />
-              <div  className='how-text2'>
+              <div className='how-text2'>
                 <h4 className='section2-h'>High Quality Listing</h4>
                 <p className='section2-p'>Detailed descriptions, professional photos and virtual tours. From a fully-equipped studio to a seaside mansion or abandoned warehouse, you can find your perfect fit.</p>
               </div>
             </div>
-            <hr className='how-hr'/>
+            <hr className='how-hr' />
             <div className='how-text'>
               <img src="images/howicon2.png" alt="" className='section2-icon' />
-              <div className='how-text2'> 
+              <div className='how-text2'>
                 <h4 className='section2-h'>Secure Payment System</h4>
                 <p className='section2-p'>With our clear rates and no hidden charges, you will know what you are paying for, right from booking, streamlined online transactions for location fees and permits.</p>
               </div>
-            </div> 
-            <hr className='how-hr'/>
+            </div>
+            <hr className='how-hr' />
             <div className='how-text'>
               <img src="images/howicon3.png" alt="" className='section2-icon' />
-              <div  className='how-text2'>
+              <div className='how-text2'>
                 <h4 className='section2-h'>Availability Calendar</h4>
                 <p className='section2-p'>Real-time availability for booking. Emergencies happen. Book locations and anything you need for any event or production with ease.</p>
               </div></div>
@@ -118,6 +119,115 @@ const Landpage = () => {
           <img src="images/how (2).png" alt="" className='section2-img' />
         </div>
       </div>
+      {/* list your space */}
+      <div className='list-space'>
+        <div className='space-section'>
+          <img src="images/list-space.png" alt="" className='space-section-img' />
+          <p className='space-section-p'>Sign up and earn money when you list your space on Skouta. The most recognised brands are filming now, be a part of their production! </p>
+          <button className='space-button'> <span>List Your Space</span></button>
+        </div>
+        <div className='space-section'>
+          <img src="images/list-space2.png" alt="" className='space-section-img' />
+          <p className='space-section-p'>We connect you with the best locations well suited to your project and your selected preference. Tell us what you’re looking for, we’re here to help!</p>
+          <button className='space-button'><span>Get Started</span></button>
+        </div>
+      </div>
+      {/* Covered section */}
+      <div className='covered-section'>
+        <div className='covered'>
+          <img src="images/Green.png" alt="" />
+          <div className='covered-text'>
+            <h1>We have you covered </h1>
+            <p>Select from our protection packages with damage protection, liability insurance, and even cancellation protection.</p>
+          </div>
+        </div>
+      </div>
+      {/* costumer reviws */}
+      <div className='costumers-review'>
+        <div className="costumer-header">
+          <h1>What our customers have to say about us</h1>
+          <p>Customer testimonials and reviews that validate the best quality locations providing social proof to potential clients.</p>
+        </div>
+        <div className='comments'>
+          <div className='comments-container'>
+            <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Suspendisse varius enim in eros </p>
+            <div className='container'>
+              <img src="images/user.png" alt="" />
+              <div className='container-text'>
+                <h4>John Smith</h4>
+                <p>Creative Director Netflix</p>
+              </div>
+            </div>
+          </div>
+          <div className='comments-containerb'>
+            <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Suspendisse varius enim in eros elementum tristique. Duis cursus, mi quis viverra ornare, eros dolor interdum nulla, ut commodo diam libero vitae erat.</p>
+            <div className='containerb'>
+              <img src="images/user.png" alt="" />
+              <div className='container-text'>
+                <h4>Funke Akindele</h4>
+                <p>Prime Video</p>
+              </div>
+
+            </div>
+          </div>
+          <div className='comments-container'>
+            <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Suspendisse varius enim in eros </p>
+            <div className='container'>
+              <img src="images/user.png" alt="" />
+              <div className='container-text'>
+                <h4>Natalie Adeniyi</h4>
+                <p>Sourcing Director EbonyLifeTV</p>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+      {/* Question */}
+      <div className='question-part'>
+        <div className='question-partA'>
+          <h1>Frequently Asked Questions (FAQs)</h1>
+          <p>Any questions? We got you.</p>
+        </div>
+        <div className='question-partB'>
+          <div className='question-section'>
+            <select name="" id="" className='questions'>
+              <option value="" className='questions-header'>
+                How do i list my space on Skouta?
+              </option>
+              {/* <option value="">Lorem ipsum dolor sit amet, consectetur adipiscing elit. Suspendisse varius enim in eros, consectetur adipiscing elit. Suspendisse</option> */}
+            </select>
+            <select name="" id="" className='questions'>
+              <option value="" className='questions-header'>How do i list my space on Skouta?</option>
+              {/* <option value="" className='question-text'>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Suspendisse varius enim in eros, consectetur adipiscing elit. Suspendisse</option> */}
+            </select>
+            <select name="" id="" className='questions'>
+              <option value="" className='questions-header'>
+                How do i list my space on Skouta?
+              </option>
+              {/* <option value="" className='question-text'>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Suspendisse varius enim in eros, consectetur adipiscing elit. Suspendisse</option> */}
+            </select>
+            <select name="" id="" className='questions'>
+              <option value="" className='questions-header'>
+                How do i list my space on Skouta?
+              </option>
+              {/* <option value="" className='question-text'>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Suspendisse varius enim in eros, consectetur adipiscing elit. Suspendisse</option> */}
+            </select>
+            <select name="" id="" className='questions'>
+              <option value="" className='questions-header'>
+                How do i list my space on Skouta?
+              </option>
+              {/* <option value="" className='question-text'>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Suspendisse varius enim in eros, consectetur adipiscing elit. Suspendisse</option> */}
+            </select>
+          </div>
+          <div className='more-question'>
+            <img src="images/message.png" alt="" />
+            <h4>Do you have more questions?</h4> 
+            <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Suspendisse varius enim in </p>
+            <button>Shoot a direct mail</button>
+          </div>
+        </div>
+      </div>
+      <Footer/>
     </div>
   )
 }
