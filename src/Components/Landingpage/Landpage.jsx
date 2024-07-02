499ff83fcc9ff60b80484162541c3e1be503f6db
timmy <akandeolawole@gmail.com>
2024-07-02 20:28:29 +0100

    respnsive starts

--- src/Components/Landingpage/Landpage.jsx
+++ src/Components/Landingpage/Landpage.jsx
@@ -6,49 +6,49 @@ const Landpage = () => {
     <div className="landing-page">
       {/* Discover section */}
       <div className="Discover w-full h-[957px] relative bg-banner bg-cover bg-center bg-no-repeat flex flex-col items-center justify-center">
-        <h1 className="text-white w-[702px] h-[192px] text-6xl font-bold text-center absolute top-[150px] leading-[96px]">
+        <h1 className="text-white text-[28px] w-[350px] sm:w-[702px] h-[192px] sm:text-6xl font-bold text-center absolute top-[77px] sm:top-[150px] sm:leading-[96px] animate-fadeInUp">
           Discover Unique Film Locations for Your Movie Production
         </h1>
-        <p className="w-[750px] h-[108px] text-center absolute top-[487px] text-white text-xl font-normal leading-9">
+        <p className="sm:w-[750px] sm:h-[108px] text-center absolute sm:top-[487px] top-[230px] mt-[px] text-[15px] w-[330px] text-white sm:text-xl font-normal leading-9 animate-fadeInUpDelay">
           Find the perfect backdrop for your film or TV project with our extensive collection of unique and diverse locations. From historic buildings to breathtaking landscapes, we have it all.
         </p>
-        <div className='w-[1153px] h-[120px] flex justify-between items-center rounded-lg absolute top-[739px] text-center p-4 bg-white'>
-          <div className='flex w-full'>
-            <input type="text" className='w-56 h-16 py-4 px-10 rounded-md border border-purple-600 ml-2' placeholder='Enter Location' />
-            <input type="text" className='w-56 h-16 py-4 px-10 rounded-md border border-purple-600 ml-2' placeholder='What are you planning?' />
-            <input type="text" className='w-56 h-16 py-4 px-10 rounded-md border border-purple-600 ml-2' placeholder='Enter Start Date' />
-            <input type="text" className='w-56 h-16 py-4 px-10 rounded-md border border-purple-600 ml-2' placeholder='Enter End Date' />
+        <div className='w-full h-[400px] sm:w-[1153px] sm:h-[120px] sm:flex justify-between items-center rounded-lg absolute top-[470px] sm:top-[739px] text-center animate-fadeInUpDelay p-4 bg-white'>
+          <div className='x1:flex w-full flex-col items-center sm:justify-center'>
+            <input type="text" className='w-[300px] p-[5px] m-[10px] h-[50px] sm:w-56 sm:h-16 sm:py-4 sm:px-10 rounded-md border border-purple-600 sm:ml-2' placeholder='Enter Location' />
+            <input type="text" className='w-[300px] p-[5px] m-[10px] h-[50px] sm:w-56 sm:h-16 sm:py-4 sm:px-10 rounded-md border border-purple-600 sm:ml-2' placeholder='What are you planning?' />
+            <input type="text" className='w-[300px] p-[5px] m-[10px] h-[50px] sm:w-56 sm:h-16 sm:py-4 sm:px-10 rounded-md border border-purple-600 sm:ml-2' placeholder='Enter Start Date' />
+            <input type="text" className='w-[300px] p-[5px] m-[10px] h-[50px] sm:w-56 sm:h-16 sm:py-4 sm:px-10 rounded-md border border-purple-600 sm:ml-2' placeholder='Enter End Date' />
           </div>
-          <button className='w-32 h-16 p-5 gap-3 rounded-lg text-white bg-purple-600 border-0'>
+          <button className='w-32 mt-[20px] h-16 p-5 gap-3 rounded-lg text-white bg-purple-600 border-0'>
             <i className="bi bi-search"></i> Search
           </button>
         </div>
       </div>
       {/* Discover section done */}
       {/* Marquee section start */}
-      <div className='w-full p-10 bg-gray-100 flex flex-col items-center mt-32'>
-        <h3 className='text-lg font-medium text-gray-600 mb-5'>Recently Shot With Us</h3>
-        <Marquee className='flex justify-around items-center w-full'>
-          <div className='w-40 m-8'>
-            <img src="images/disney_logo.svg.png" alt="brand" />
+      <div className='h-[113] w-full sm:w-full p-10 bg-gray-100 flex flex-col items-center mt-32'>
+      <h3 className='text-[20px] sm:text-lg font-medium text-gray-600 mb-5 animate-fadeInUp animate-fadeInUpDelay'>Recently Shot With Us</h3>
+        <Marquee className='flex justify-between items-center w-full '>
+          <div className=' '>
+            <img src="images/disney_logo.svg.png" alt="brand" className='w-[122px] h-[24px] x1:w-40 x1:m-8 ml-[50px]'/>
           </div>
-          <div className='w-40 m-8'>
-            <img src="images/ebonylife_studios_logo.png.png" alt="brand" />
+          <div className=' sm:w-40 sm:m-8'>
+            <img src="images/ebonylife_studios_logo.png.png" alt="brand" className='w-[122px] h-[24px] x1:w-40 x1:m-8 ml-[50px]'/>
           </div>
-          <div className='w-40 m-8'>
-            <img src="images/filmancc_logo.png.png" alt="brand" />
+          <div className=' sm:w-40 sm:m-8'>
+            <img src="images/filmancc_logo.png.png" alt="brand" className='w-[122px] h-[24px] x1:w-40 x1:m-8 ml-[50px]'/>
           </div>
-          <div className='w-40 m-8'>
-            <img src="images/Logo.png.png" alt="brand" />
+          <div className=' sm:w-40 sm:m-8'>
+            <img src="images/Logo.png.png" alt="brand" className='w-[122px] h-[24px] sm:w-40 x1:m-8 x1-[50px] ml-[50px]'/>
           </div>
-          <div className='w-40 m-8'>
-            <img src="images/netflix_logo.svg.png" alt="brand" />
+          <div className=' sm:w-40 sm:m-8'>
+            <img src="images/netflix_logo.svg.png" alt="brand" className='w-[122px] h-[24px] x1:w-40 xl:m-8 x1-[50px] ml-[50px]'/>
           </div>
-          <div className='w-40 m-8'>
-            <img src="images/nollywood_gists_logo.png.png" alt="brand" />
+          <div className=' sm:w-40 sm:m-8'>
+            <img src="images/nollywood_gists_logo.png.png" alt="brand" className='w-[122px] h-[24px] x1:w-40 x1:m-8 x1-[50px] ml-[50px]' />
           </div>
-          <div className='w-40 m-8'>
-            <img src="images/tecnomobile_logo.svg.png" alt="brand" />
+          <div className='sm:w-40 sm:m-8'>
+            <img src="images/tecnomobile_logo.svg.png" alt="brand" className='w-[122px] h-[24px] x1:w-40 x1:m-8 x1-[5px] ml-[50px]'/>
           </div>
         </Marquee>
       </div>
@@ -62,25 +62,25 @@ const Landpage = () => {
       {/* Created by skoukar */}
       <div className='w-full py-24 bg-white mt-10'>
         <div className='text-center mb-10'>
-          <h1 className='text-4xl font-bold leading-6 text-gray-900'>Created With Skoutar</h1>
-          <p className='text-base font-normal leading-6 text-gray-600 mx-auto mt-5'>Better Locations, Endless Possibilities</p>
+          <h1 className='text-[28px] sm:text-[60px] font-bold leading-6 text-gray-900'>Created With Skoutar</h1>
+          <p className='text-[18px] mt-[10px] sm:text-base font-normal leading-6 text-gray-600 mx-auto sm:mt-10'>Better Locations, Endless Possibilities</p>
         </div>
-        <div className='flex flex-wrap justify-center items-start mt-[80px]'>
-          <div>
+        <div className='flex items-center ml-[10px] w-full sm:mt-[80px]'>
+          <div className='flex flex-col items-center justify-center'>
             <div className='max-w-2xl mx-auto mt-10 text-left'>
-              <h4 className='text-2xl font-bold leading-6 text-gray-900 transition-colors duration-300 hover:text-indigo-600 cursor-pointer'>Discover Amazing Film Locations</h4>
-              <p className='text-base leading-normal text-gray-700 mt-5'>Our advanced search filters allow you to find the perfect film location for your next project. Explore verified locations with ease and read user reviews to make informed decisions.</p>
+              <h4 className='text-[20px] w-[170px] sm:w-auto sm:text-2xl font-bold leading-6 text-gray-900 transition-colors duration-300 hover:text-indigo-600 cursor-pointer'>Discover Amazing Film Locations</h4>
+              <p className='text-[16px] sm:text-base leading-normal text-gray-700 mt-5 w-[170px] sm:w-[586px]'>Our advanced search filters allow you to find the perfect film location for your next project. Explore verified locations with ease and read user reviews to make informed decisions.</p>
             </div>
             <div className='max-w-2xl mx-auto mt-10 text-left'>
-              <h4 className='text-2xl font-bold leading-6 text-gray-900 transition-colors duration-300 hover:text-indigo-600 cursor-pointer'>Verified Locations</h4>
-              <p className='text-base leading-normal text-gray-700 mt-5'>We ensure that all our film locations are verified to meet the highest standards. You can trust that the locations listed on our platform are reliable and suitable for your needs.</p>
+              <h4 className='text-[20px] w-[170px] sm:w-auto sm:text-2xl font-bold leading-6 text-gray-900 transition-colors duration-300 hover:text-indigo-600 cursor-pointer'>Verified Locations</h4>
+              <p className='text-[16px] sm:text-base leading-normal text-gray-700 mt-5 w-[170px] sm:w-[586px]'>We ensure that all our film locations are verified to meet the highest standards. You can trust that the locations listed on our platform are reliable and suitable for your needs.</p>
             </div>
             <div className='max-w-2xl mx-auto mt-10 text-left'>
-              <h4 className='text-2xl font-bold leading-6 text-gray-900 transition-colors duration-300 hover:text-indigo-600 cursor-pointer'>User Reviews</h4>
-              <p className='text-base leading-normal text-gray-700 mt-5'>Read honest and unbiased reviews from other filmmakers who have used our platform. Their experiences will help you make the right choice for your film production.</p>
+              <h4 className='text-[20px] w-[170px] sm:w-auto sm:text-2xl font-bold leading-6 text-gray-900 transition-colors duration-300 hover:text-indigo-600 cursor-pointer'>User Reviews</h4>
+              <p className='text-[16px] sm:text-base leading-normal text-gray-700 mt-5 w-[170px] sm:w-[586px]'>Read honest and unbiased reviews from other filmmakers who have used our platform. Their experiences will help you make the right choice for your film production.</p>
             </div>
           </div>
-          <img src="images/created.png" alt="" className="w-[628px] h-[588px] rounded-lg m-5" />
+          <img src="images/created.png" alt="" className="w-[190px] h-[908px] sm:w-[628px] sm:h-[588px] rounded-lg m-5" />
         </div>
 
       </div>
@@ -91,7 +91,7 @@ const Landpage = () => {
         <div className='relative flex w-[1280px] h-[944px]'>
           <div className='w-[672px] h-[782px] absolute top-[81px] bg-[rgb(248,248,248)] rounded-lg shadow-[0px_2px_2px_2px_#22222214]'>
             <div className='flex w-[594px] h-[174px] ml-[15px] mt-[70px]'>
-              <img src="images/howicon1.png" alt="" className='w-[64px] h-[64px] text-[#6B6B6B] mt-[50px]'/>
+              <img src="images/howicon1.png" alt="" className='w-[64px] h-[64px] text-[#6B6B6B] mt-[50px]' />
               <div className='w-[490px] h-[174px] ml-[20px]'>
                 <h4 className='w-[490px] h-[42px] text-[28px] font-bold leading-[42px] text-left text-[#6200EE]'>High Quality Listing</h4>
                 <p className='w-[490px] h-[120px] text-[15px] font-normal leading-[30px] text-left mt-[5px]'>Detailed descriptions, professional photos and virtual tours. From a fully-equipped studio to a seaside mansion or abandoned warehouse, you can find your perfect fit.</p>
@@ -146,30 +146,30 @@ const Landpage = () => {
           <p className='w-[700px] mt-4 text-center font-normal leading-10 text-gray-700'>Customer testimonials and reviews that validate the best quality locations providing social proof to potential clients.</p>
         </div>
         <div className='w-1206 h-auto flex'>
-          <div className='w-[500px] h-[300px] p-[32px] bg-purple-600 ml-5'>
+          <div className='w-[400px] h-[300px] p-[32px] bg-purple-600 ml-5'>
             <p className='w-[362px] h-[90px] text-base font-normal leading-30 text-left'>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Suspendisse varius enim in eros </p>
             <div className='w-[293px] h-[78px] flex mt-[45px]'>
-              <img src="images/user.png" alt="" className='w-[64px] h-[64px] border-[2px] border-white bg-white p-[10px] rounded-full mt-[10px]'/>
+              <img src="images/user.png" alt="" className='w-[64px] h-[64px] border-[2px] border-white bg-white p-[10px] rounded-full mt-[10px]' />
               <div className='w-[400px] ml-5 h-[78px] my-[12px]'>
                 <h4 className='w-[200px] h-[36px] text-2xl font-semibold leading-36 text-gray-900'>John Smith</h4>
                 <p className='text-base font-normal leading-30text-left'>Creative Director Netflix</p>
               </div>
             </div>
           </div>
-          <div className='w-[500px] h-[400px] p-[32px] bg-purple-600 ml-5'>
-            <p className='w-[362px] h-[90px] text-base font-normal leading-30 text-left'>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Suspendisse varius enim in eros elementum tristique. Duis cursus, mi quis viverra ornare, eros dolor interdum nulla, ut commodo diam libero vitae erat.</p>
+          <div className='w-[386px] h-[400px] p-[32px] bg-purple-600 ml-5'>
+            <p className='w-[360px] h-[90px] text-base font-normal leading-30 text-left'>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Suspendisse varius enim in eros elementum tristique. Duis cursus, mi quis viverra ornare, eros dolor interdum nulla, ut commodo diam libero vitae erat.</p>
             <div className='w-[293px] h-[78px] flex mt-[45px]'>
-              <img src="images/user.png" alt="" className='w-[64px] h-[64px] border-[2px] border-white bg-white p-[10px] rounded-full mt-[10px]'/>
+              <img src="images/user.png" alt="" className='w-[64px] h-[64px] border-[2px] border-white bg-white p-[10px] rounded-full mt-[10px]' />
               <div className='w-[400px] ml-5 h-[78px] my-[12px]'>
                 <h4 className='w-[200px] h-[36px] text-2xl font-semibold leading-36 text-gray-900'>Funke Akindele</h4>
                 <p className='text-base font-normal leading-30text-left'>Prime Video</p>
               </div>
             </div>
           </div>
-          <div className='w-[500px] h-[300px] p-[32px] bg-purple-600 ml-5'>
+          <div className='w-[386px] h-[300px] p-[32px] bg-purple-600 ml-5'>
             <p className='w-[362px] h-[90px] text-base font-normal leading-30 text-left'>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Suspendisse varius enim in eros </p>
             <div className='w-[293px] h-[78px] flex mt-[45px]'>
-              <img src="images/user.png" alt="" className='w-[64px] h-[64px] border-[2px] border-white bg-white p-[10px] rounded-full mt-[10px]'/>
+              <img src="images/user.png" alt="" className='w-[64px] h-[64px] border-[2px] border-white bg-white p-[10px] rounded-full mt-[10px]' />
               <div className='w-[400px] ml-5 h-[78px] my-[12px]'>
                 <h4 className='w-[200px] h-[36px] text-2xl font-semibold leading-36 text-gray-900'>Natalie Adeniyi</h4>
                 <p className='text-base font-normal leading-30text-left'>Sourcing Director EbonyLifeTV</p>
@@ -216,29 +216,29 @@ const Landpage = () => {
             </select>
           </div>
           <div className='w-[1000px] h-[500px] top-[386px] left-[949px] bg-white p-[21px] flex flex-col items-center justify-around'>
-            <img src="images/message.png" alt="" className='w-[100px] h-[100px]'/>
-            <h4 className='w-[350px] text-[28px] font-bold leading-[px] text-center'>Do you have more questions?</h4> 
+            <img src="images/message.png" alt="" className='w-[100px] h-[100px]' />
+            <h4 className='w-[350px] text-[28px] font-bold leading-[px] text-center'>Do you have more questions?</h4>
             <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Suspendisse varius enim in </p>
             <button className='w-[228px] h-auto p-[20px_8px] gap-[8px] rounded-[8px] bg-[#6200EE] text-white'>Shoot a direct mail</button>
           </div>
         </div>
       </div>
       {/* Stay Update */}
-      <div className='bg-white w-full h-[500px] flex items-center justify-center'> 
+      <div className='bg-white w-full h-[500px] flex items-center justify-center'>
         <div className='flex items-center'>
           <div className='updated-A w-[586px] h-auto mr-[40px]'>
             <h4 className='w-[586px] text-[40px] font-bold leading-[60px] text-left'>Stay Updated with Skouta’s Newsletter</h4>
             <p className='w-[586px] text-[20px] font-normal leading-[36px] text-left'>Find Your Perfect Film Location. Discover unique and stunning film locations for your next project</p>
           </div>
-          <div className='w-[639px] ml-[40px]'> 
+          <div className='w-[639px] ml-[40px]'>
             <p className='w-[628px] text-[20px] font-normal leading-[36px] text-left'>Subscribe to our newsletter for the latest updates, special offers, and to join us on our journey.</p>
             <div className='form-input w-[639px] h-[64px] relative mt-[20px] rounded-[8px]'>
-              <input type="text" placeholder='Your Email Address' className='w-[411px] h-auto p-[22px_256px_18px_16px] rounded-[8px] bg-[#E2E2E2]'/>
+              <input type="text" placeholder='Your Email Address' className='w-[411px] h-auto p-[22px_256px_18px_16px] rounded-[8px] bg-[#E2E2E2]' />
               <button className='w-[228px] h-auto p-[20px_8px] gap-[8px] rounded-[8px] bg-[#6200EE] text-white'>Subcribe</button>
               <p className='w-[628px] text-[20px] font-normal leading-[36px] text-left mt-[10px]'>By subscribing, you agree to our Terms and Conditions.</p>
             </div>
           </div>
-        </div>       
+        </div>
       </div>
     </div>
   )
--- tailwind.config.js
+++ tailwind.config.js
@@ -2,8 +2,10 @@ import tailwindScrollbar from 'tailwind-scrollbar';
 
 /** @type {import('tailwindcss').Config} */
 export default {
-  content: ["./index.html",
-    "./src/**/*.{js,ts,jsx,tsx}",],
+  content: [
+    "./index.html",
+    "./src/**/*.{js,ts,jsx,tsx}",
+  ],
   theme: {
     extend: {
       backgroundImage: {
@@ -13,10 +15,24 @@ export default {
       colors: {
         mainColor: '#6200EE',
       },
+      keyframes: {
+        fadeInUp: {
+          '0%': { opacity: '0', transform: 'translateY(20px)' },
+          '100%': { opacity: '1', transform: 'translateY(0)' },
+        },
+        slide: {
+          '0%': { transform: 'translateX(100%)' },
+          '100%': { transform: 'translateX(-100%)' },
+        },
+      },
+      animation: {
+        fadeInUp: 'fadeInUp 1s ease-in-out',
+        fadeInUpDelay: 'fadeInUp 1.5s ease-in-out',
+        slide: 'slide 10s linear infinite',
+      },
     },
   },
   plugins: [
     tailwindScrollbar,
   ],
-
 };
